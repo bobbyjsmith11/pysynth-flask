66edd93621a20459729c983e0401b9d952bfa5b5
Bobby Smith <bobby@epiqsolutions.com>
2018-04-25 14:18:12 -0500

    can test the lo filter remotely# Please enter the commit message for your changes. Lines starting

--- pysynth/remote.py
+++ pysynth/remote.py
@@ -18,6 +18,7 @@ PORT = '5000'
 
 
 def tune_lo(rf_freq_Hz, ip=IP_ADDR, port=PORT):
+    """ tune the LO """
     rf_freq_GHz = rf_freq_Hz/1e9
     dat = {'freq': rf_freq_GHz} 
     res = requests.get('http://' + ip + ':' + port +'/tune_lo', json=dat)
@@ -27,23 +28,14 @@ def tune_lo(rf_freq_Hz, ip=IP_ADDR, port=PORT):
         raise Exception('tune_lo to ' + str(rf_freq_Hz) + ' Hz unsuccessful')
 
 def check_lock_detect(ip=IP_ADDR, port=PORT):
+    """ read back the status of the lock detect
+    """
     res = requests.get('http://' + ip + ':' + port +'/check_lock_detect')
     if res.ok:
         return res.json()['locked']
 
-
-def capture_s2p(filename, ip_addr='192.168.2.41', port=5001):
-    vna = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    vna.connect((ip_addr, port))
-    dir = '\'C:\\Users\\bsmith\\'
-
-    # time.sleep(7)
-    cmd_str = ":MMEM:STOR " + dir + filename + '.s2p\'\n'
-    print(cmd_str)
-    vna.send(str.encode(cmd_str))
-
 def set_lo_filter(band, ip=IP_ADDR, port=PORT):
-    """
+    """ set the LO filter band
     """
     dat = {'band': band} 
     res = requests.get('http://' + ip + ':' + port +'/set_lo_filter', json=dat)
@@ -51,3 +43,18 @@ def set_lo_filter(band, ip=IP_ADDR, port=PORT):
         return res.json()
     else:
         raise Exception('set_lo_filter to ' + str(band) + ' Hz unsuccessful')
+
+def test_all_filter_bands():
+    """ test all of the 32 filter bands for the AM3043 """
+    for i in range(32):
+        set_lo_filter(i)
+        time.sleep(3)
+        capture_s2p('State' + str(i))
+
+def capture_s2p(filename, ip_addr='192.168.2.41', port=5001):
+    """ cappture s2p file and store in your home directory """
+    vna = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    vna.connect((ip_addr, port))
+    dir = '\'C:\\Users\\bsmith\\'
+    cmd_str = ":MMEM:STOR " + dir + filename + '.s2p\'\n'
+    vna.send(str.encode(cmd_str))
