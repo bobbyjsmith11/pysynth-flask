0a2e40e55b4819d0390c88a6fddc1dec24c69f8a
bobbyjsmith11 <bobbyjsmith11@gmail.com>
2018-06-03 19:39:32 -0500

    added the ADI python driver for the ADF5355 and my own version for the ADF5356

---
+++ pysynth/test_adf5355.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+__author__ = 'Qyon'
+
+
+if __name__ == '__main__':
+    from adf5355 import adf5355
+    import spidev
+    from time import sleep
+    spi = spidev.SpiDev()
+    # spi.open(1, 0)
+    spi.open(0, 0)
+    # spi.max_speed_hz = 4000000
+    spi.max_speed_hz = 7629
+    d = adf5355(spi, mute_till_lock_detect_en=True, ref_doubler_en=False, clkin=122880000, outb_en=True, outa_en=True, outb_power=3)
+    f0 = 10368900000
+    x = 0
+    xmax = 10
+    xdir = 1
+    fstep = 200
+    sleep_time = 0.5
+    sleep_time_step = -0.1
+    while True:
+        x += xdir
+        if x < 0:
+            x = 1
+            xdir = 1
+            sleep_time += sleep_time_step
+            if sleep_time <= 0:
+                sleep_time = 0.5
+        elif x > xmax:
+            x = xmax
+            xdir = -1
+        d.set_freq(f0 + x * fstep, 1)
+        sleep(sleep_time)
